35298da36ba9c9df4df33f6b9620c1dda3fdcde0
admov <admin@mov.lt>
2014-04-07 15:36:34 +0200

    added additional cookbooks to my_cookbook

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -6,5 +6,8 @@ description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'build-essential'
+depends          'chef-client'
+depends          'apt'
+depends          'ntp'
 depends          'apache2', '>= 1.0.4'
 conflicts        'nginx'
